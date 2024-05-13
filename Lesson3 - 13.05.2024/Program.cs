9d861dec797243292423a906620968cefca34ba8
Bayramli Abbas <abbascb@code.local>
2024-05-13 17:25:19 +0400

    Lesson 13.05.2024

---
+++ Lesson3 - 13.05.2024/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Lesson3___13._05._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            //while (true)
+            //{
+            //    Console.Write("Necenci ay oldugunu yazin: ");
+            //    int month = Convert.ToInt32(Console.ReadLine());
+
+            //    switch (month)
+            //    {
+            //        case 1:
+            //            Console.WriteLine("Yanvar");
+            //            break;
+            //        case 2:
+            //            Console.WriteLine("Fevral");
+            //            break;
+            //        case 3:
+            //            Console.WriteLine("Mart");
+            //            break;
+            //        case 4:
+            //            Console.WriteLine("Aprel");
+            //            break;
+            //        case 5:
+            //            Console.WriteLine("May");
+            //            break;
+            //        case 6:
+            //            Console.WriteLine("Iyun");
+            //            break;
+            //        case 7:
+            //            Console.WriteLine("Iyul");
+            //            break;
+            //        case 8:
+            //            Console.WriteLine("Avqust");
+            //            break;
+            //        case 9:
+            //            Console.WriteLine("Sentyabr");
+            //            break;
+            //        case 10:
+            //            Console.WriteLine("Oktyabr");
+            //            break;
+            //        case 11:
+            //            Console.WriteLine("Noyabr");
+            //            break;
+            //        case 12:
+            //            Console.WriteLine("Dekabr");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Bele bir ay yoxdur !");
+            //            break;
+            //    }
+            //}
+            #endregion
+
+            #region Task 2
+            //int i = 1;
+            //int sum = 0;
+
+            //while (i <= 100)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        sum += i;
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+            //Console.WriteLine("1-den 100-e cut ededlerin cemi: " + sum);
+            #endregion
+
+            #region Task 3
+            //int sum = 0;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i%2==0)
+            //    {
+            //        Console.WriteLine(i);
+            //        sum += i;
+            //    }
+            //}
+            //Console.WriteLine("1-den 100-e cut ededlerin cemi: " + sum);
+            #endregion
+        }
+    }
+}
